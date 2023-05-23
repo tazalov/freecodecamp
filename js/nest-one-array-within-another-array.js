6886e797ba78cc0f8189b2b7b7863e82f2a13a38
tazalov <tazalov9@gmail.com>
2023-05-23 10:08:33 +0300

    Add solution course 'Basic JavaScript' 39-50

---
+++ js/nest-one-array-within-another-array.js
@@ -0,0 +1,2 @@
+// Only change code below this line
+const myArray = [['balls', 3], ['graph', 2]];
---
+++ js/shopping-list.js
@@ -0,0 +1,6 @@
+const myList = [['milk', 2], 
+                ['potato', 10],
+                ['cheese', 5],
+                ['chocolate', 7],
+                ['cucumber', 15]
+                ];
