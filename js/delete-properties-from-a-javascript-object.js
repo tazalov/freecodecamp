871c39bad49d897659fafd623e7ca7a73d16cf07
tazalov <tazalov9@gmail.com>
2023-05-26 10:15:33 +0300

    Add solution course 'Basic JavaScript' 81-90

---
+++ js/delete-properties-from-a-javascript-object.js
@@ -0,0 +1,15 @@
+/*
+Add a bark property to myDog and set it to a dog sound, such as "woof". You may use either dot or bracket notation.
+*/
+
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails
