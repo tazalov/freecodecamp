f1069998d5bc8124d3482eeaa5dc19be2368de2f
tazalov <tazalov9@gmail.com>
2023-05-27 11:48:43 +0300

    Solution of lessons 7-12 "Basic Data Structures"

---
+++ js/basicStructureData/copy-array-items-using-slice.js
@@ -0,0 +1,11 @@
+/*
+We have defined a function, forecast, that takes an array as an argument. Modify the function using slice() to extract information from the argument array and return a new array that contains the string elements warm and sunny.
+*/
+
+function forecast(arr) {
+  // Only change code below this line
+  return arr.slice(2,4);
+}
+
+// Only change code above this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
