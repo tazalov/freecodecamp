8cc77485433399bd06937b6a0452b4fa833a1211
tazalov <tazalov9@gmail.com>
2023-05-27 09:51:54 +0300

    Solution of lessons 1-6 "Basic Data Structures"

---
+++ js/basicStructureData/remove-items-using-splice.js
@@ -0,0 +1,12 @@
+/*
+We've initialized an array arr. Use splice() to remove elements from arr, so that it only contains elements that sum to the value of 10.
+*/
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(0,2);
+arr.splice(1,2);
+arr.splice(2,2);
+
+// Only change code above this line
+console.log(arr);
---
+++ js/basicStructureData/use-an-array-to-store-a-collection-of-data.js
@@ -0,0 +1,5 @@
+/*
+We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. Your array should contain at least one string, one number, and one boolean.
+*/
+
+let yourArray = ['one', 2, 'three', true, false, undefined, null]; // Change this line
