e7c7f0a4e8c1d9fd7bd27e7092c92a26c5eae747
tazalov <tazalov9@gmail.com>
2023-06-03 10:49:56 +0300

    Solution of lessons 24-26 "Object Oriented Programming"

---
+++ js/OOP/use-an-iife-to-create-a-module.js
@@ -0,0 +1,29 @@
+/*
+Create a module named funModule to wrap the two mixins isCuteMixin and singMixin. funModule should return an object.
+*/
+
+let isCuteMixin = function(obj) {
+  obj.isCute = function() {
+    return true;
+  };
+};
+let singMixin = function(obj) {
+  obj.sing = function() {
+    console.log("Singing to an awesome tune");
+  };
+};
+
+const funModule = (function () {
+  return {
+    isCuteMixin: function (obj) {
+      obj.isCute = function () {
+        return true;
+      };
+    },
+    singMixin: function (obj) {
+      obj.sing = function () {
+        console.log('Singing to an awesome tune');
+      };
+    },
+  };
+})();
