925258ab0f93e027a9ba6f814ade8bc3a30a82dd
tazalov <tazalov9@gmail.com>
2023-06-05 09:38:10 +0300

    Add solution 6-10 'Regular Expressions'

---
+++ js/regularExpression/find-more-than-the-first-match.js
@@ -0,0 +1,7 @@
+/*
+Using the regex starRegex, find and extract both Twinkle words from the string twinkleStar.
+*/
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
