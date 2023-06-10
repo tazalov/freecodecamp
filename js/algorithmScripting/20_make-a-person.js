d4a01b6f3ac82486049467c5f511e05fc2925b9b
tazalov <tazalov9@gmail.com>
2023-06-10 08:28:06 +0300

    Add solution 20 'Algorithm Scripting'

---
+++ js/algorithmScripting/20_make-a-person.js
@@ -0,0 +1,41 @@
+/*
+Fill in the object constructor with the following methods below:
+
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
+Run the tests to see the expected output for each method. The methods that take an argument must accept only one argument and it has to be a string. These methods must be the only available means of interacting with the object.
+*/
+
+const Person = function(firstAndLast) {
+  let fullName = firstAndLast;
+
+  this.getFirstName = function() {
+    return fullName.split(" ")[0];
+  };
+
+  this.getLastName = function() {
+    return fullName.split(" ")[1];
+  };
+
+  this.getFullName = function() {
+    return fullName;
+  };
+
+  this.setFirstName = function(first) {
+    fullName = first + " " + fullName.split(" ")[1];
+  };
+
+  this.setLastName = function(last) {
+    fullName = fullName.split(" ")[0] + " " + last;
+  };
+
+  this.setFullName = function(firstAndLast) {
+    fullName = firstAndLast;
+  };
+};
+
+const bob = new Person("Bob Ross");
